a94da930211a47c621825b14c29317eda5dea19f
Jeevesh0207 <r27abbit@gmail.com>
2024-08-12 14:22:41 +0530

    first commit

---
+++ frontend/src/hooks/useGetTrendingContent.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useContentStore } from "../store/content";
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_URL;
+
+const useGetTrendingContent = () => {
+  const [trendingContent, setTrendingContent] = useState(null);
+  const { contentType } = useContentStore();
+
+  useEffect(() => {
+    const getTrendingContent = async () => {
+      try {
+        const res = await axios.get(BASE_URL + `/api/v1/${contentType}/trending`, {
+          withCredentials: true, // Include credentials (cookies) with the request
+        });
+        setTrendingContent(res.data.content);
+      } catch (error) {
+        console.error("Error fetching trending content:", error);
+      }
+    };
+
+    getTrendingContent();
+  }, [contentType]);
+
+  console.log({ trendingContent });
+
+  return { trendingContent };
+};
+
+export default useGetTrendingContent;
---
+++ frontend/src/store/authUser.js
@@ -0,0 +1,73 @@
+import axios from "axios";
+import toast from "react-hot-toast";
+import { create } from "zustand";
+import Cookies from 'js-cookie';  // Import js-cookie
+
+const BASE_URL = import.meta.env.VITE_URL
+
+export const useAuthStore = create((set) => ({
+	user: null,
+	isSigningUp: false,
+	isCheckingAuth: true,
+	isLoggingOut: false,
+	isLoggingIn: false,
+	signup: async (credentials) => {
+		set({ isSigningUp: true });
+		try {
+			const response = await axios.post(BASE_URL + "/api/v1/auth/signup", credentials);
+			if (response.data.token) {
+				Cookies.set('jwt-netflix', response.data.token, { expires: 7 }); // Adjust expiration as needed
+			}
+			set({ user: response.data.user, isSigningUp: false });
+			toast.success("Account created successfully");
+		} catch (error) {
+			toast.error(error.response.data.message || "Signup failed");
+			set({ isSigningUp: false, user: null });
+		}
+	},
+	login: async (credentials) => {
+		set({ isLoggingIn: true });
+		try {
+			const response = await axios.post(BASE_URL + "/api/v1/auth/login", credentials);
+
+			// Set the token in cookies
+			if (response.data.token) {
+				Cookies.set('jwt-netflix', response.data.token, { expires: 7 }); // Adjust expiration as needed
+			}
+
+			set({ user: response.data.user, isLoggingIn: false });
+		} catch (error) {
+			set({ isLoggingIn: false, user: null });
+			toast.error(error.response.data.message || "Login failed");
+		}
+	},
+	logout: async () => {
+		set({ isLoggingOut: true });
+		try {
+			await axios.post(BASE_URL + "/api/v1/auth/logout");
+			Cookies.remove('authToken'); // Remove the token from cookies
+			set({ user: null, isLoggingOut: false });
+			toast.success("Logged out successfully");
+		} catch (error) {
+			set({ isLoggingOut: false });
+			toast.error(error.response.data.message || "Logout failed");
+		}
+	},
+	authCheck: async () => {
+		set({ isCheckingAuth: true });
+		try {
+			const token = Cookies.get('authToken'); // Retrieve token from cookies
+			if (token) {
+				const response = await axios.get(BASE_URL + "/api/v1/auth/authCheck", {
+					headers: { 'Authorization': `Bearer ${token}` }
+				});
+				set({ user: response.data.user, isCheckingAuth: false });
+			} else {
+				set({ isCheckingAuth: false, user: null });
+			}
+		} catch (error) {
+			set({ isCheckingAuth: false, user: null });
+			// toast.error(error.response.data.message || "An error occurred");
+		}
+	},
+}));
