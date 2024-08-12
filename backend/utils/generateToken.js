6a5e515846035f6602c51b5e3a7c66c5cca702b5
Jeevesh0207 <r27abbit@gmail.com>
2024-08-12 16:29:28 +0530

    "Comment out httpOnly, sameSite, and secure options in generateTokenAndSetCookie function"

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -6,12 +6,12 @@ export const generateTokenAndSetCookie = (userId, res) => {
 
 	res.cookie("jwt-netflix", token, {
 		maxAge: 15 * 24 * 60 * 60 * 1000,
-		httpOnly: true, 
-		sameSite: 'None',
-		secure: true,
+		// httpOnly: true, 
+		// sameSite: 'None',
+		// secure: true,
 	});
 
-	
+
 	return {
 		ok: true,
 		token,
