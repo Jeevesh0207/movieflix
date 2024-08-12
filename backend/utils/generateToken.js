3368e4511afcf8cf95ef32ad4fc9be449e5f2acc
Jeevesh0207 <r27abbit@gmail.com>
2024-08-12 15:41:21 +0530

    modify cookies

--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -4,15 +4,21 @@ import { ENV_VARS } from "../config/envVars.js";
 export const generateTokenAndSetCookie = (userId, res) => {
 	const token = jwt.sign({ userId }, ENV_VARS.JWT_SECRET, { expiresIn: "15d" });
 
-	res.cookie("jwt-netflix", token, {
-		maxAge: 15 * 24 * 60 * 60 * 1000,
-		httpOnly: true, 
-		sameSite: 'None',
-		secure: true,
+	// res.cookie("jwt-netflix", token, {
+	// 	maxAge: 15 * 24 * 60 * 60 * 1000,
+	// 	httpOnly: true, 
+	// 	sameSite: 'None',
+	// 	secure: true,
+	// });
+
+	res.cookie('jwt-netflix', token, {
+		httpOnly: true, // Makes cookie inaccessible to JavaScript
+		secure: process.env.NODE_ENV === 'production', // Ensures cookies are only sent over HTTPS
+		sameSite: 'strict', // Controls whether cookies are sent with cross-site requests
 	});
 
 	return {
-		ok:true,
+		ok: true,
 		token,
 	}
 };
